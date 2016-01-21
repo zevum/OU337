f6d0dc042d1ec002ff28f73dedaa3b3919238c04
Brandon Smith <zuum@comcast.net>
2016-01-21 17:04:13 -0500

    Added new method to calculate binary2decimal

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+
 import java.util.Scanner;
 
 public class Main {
+
+    public static int Binary2Decimal(String binary) {
+
+
+        int result = 0; // Temporarily holds result
+        for (int i = 0; i < binary.length(); i++)
+
+        {
+            int digit;
+            digit = Character.getNumericValue(binary.charAt(i));
+            int position = binary.length() - i - 1;
+            result += digit * Math.pow(2, position);
+
+        }
+        return result;
+
+    }
 
     public static void main(String[] args)
     {
@@ -10,5 +28,9 @@ public class Main {
         String buffer = scanner.nextLine();
         System.out.println("You entered: " + buffer + " for conversion.\n");
         System.out.println("Proceeding the data ....\n");
+        System.out.println(Binary2Decimal(buffer));
+
     }
+
+
 }
